02acb2ac75ca01214426b5782d824439d12888c5
israrvz <israel.ramos@alumno.uttehuacan.edu.mx>
2018-09-06 15:44:29 -0500

    September 6th

---
+++ ComputeChange.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class ComputeChange{
+public static void main(String[] args) {
+Scanner input = new Scanner (System.in);
+System.out.println("Enter an amount in double:");
+double amount = input.nextDouble();
+int cents = (int)(amount * 100);
+//find the number dollars
+int numberOfDollars = cents / 100;
+cents = cents % 100;
+//find the quartes
+int numberOfQuartes = cents /25;
+cents = cents % 25;
+// number of dimes
+int numberOfDimes = cents / 10;
+// number of nickels
+int numberOfNickels = cents / 5;
+cents = cents % 5;
+// number of pennis
+int numberOfPennies = cents;
+// mostrar resultados
+System.out.println("Your amount" + amount + "consist of \n" +
+"\t" + numberOfDollars + "dollars" +
+"\t" + numberOfQuartes + "quartes" +
+"\t" + numberOfDimes + "dimes" +
+"\t" + numberOfNickels + "nickels" +
+"\t" + numberOfPennies + "pennis");
+}
+}
