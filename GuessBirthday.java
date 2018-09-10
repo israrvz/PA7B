924815c1540b3e906b67ba420db8901a9465dcc7
israrvz <israel.ramos@alumno.uttehuacan.edu.mx>
2018-09-10 17:17:20 -0500

    September 10

---
+++ GuessBirthday.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class GuessBirthday {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    String set1="1 3 5 7 \n"+
+                "9 11 13 15\n"+
+                "17 19 21 23\n"+
+                "25 27 29 31\n";
+    String set2="2 3 6 7 \n"+
+                "10 11 14 15\n"+
+                "18 19 22 23\n"+
+                "26 27 30 31\n";
+    String set3="4 5 6 7 \n"+
+                "12 13 14 15\n"
+                +"20 21 22 23\n"
+                +"28 29 30 31\n";
+    String set4="8 9 10 11 \n"+
+                "12 13 14 15\n"+
+                "24 25 26 27\n"+
+                "28 29 30 31\n";
+    String set5="16 17 18 19 \n"+
+                "20 21 22 23\n"+
+                "24 25 26 27\n"+
+                "28 29 30 31\n";
+    
+  }
+}
